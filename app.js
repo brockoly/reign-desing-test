ad4c9d8d7e68ed595434dac87b390b4ae571f9e3
Andres Meneses <ameneses@snuuper.com>
2018-08-24 01:14:18 -0300

    removing static express pages and adding opening of url on row click

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
-app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.use(bodyParser.json());
 require('./routes')(app);
 
--- public/main.js
+++
@@ -1,6 +0,0 @@
-const news = require('../controllers/newsController');
-// return await news.disableArticle(id);
-const disableArticle = async (id) => {
-  // console.log('entra', id);
-  return await news.disableArticle(id);
-}
